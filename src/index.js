bddf46db17112c9c83d16e55fb5e1d0e4ad0d175
Jonas B. Bernardi <jonas.bernardi@duosystem.com.br>
2025-05-02 12:32:17 -0300

    feat: index + backup service

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const { getBackup, postBackup } = require('./services/backup.service');
+require('dotenv').config();
+
+console.log(`Uncomment lines bellow line 4 in ${__filename}`);
+
+// getBackup('healthbot.bkp');
+// postBackup('healthbot.bkp');
---
+++ src/services/backup.service.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+const fs = require('fs');
+const { generateToken } = require('./jwt.service');
+const { getTenantName, getBackupUrl } = require('./tenant.service');
+
+const getBackup = async (backupFile) => {
+  try{
+    const tenantName = getTenantName();
+    const backupUrl = getBackupUrl(tenantName);
+    const authToken = generateToken(tenantName);
+
+    const res = await axios.get(backupUrl, {
+      headers: {
+        'Authorization': 'Bearer ' + authToken
+      }
+    });
+
+    const backup = res.data;
+    fs.writeFileSync(backupFile, backup, {encoding: 'utf-8'});
+  } catch (error) {
+    const message = error.response?.data || error.message;
+    console.error(message);
+  }
+}
+
+const postBackup = async (backupFile) => {
+  try{
+    const tenantName = getTenantName();
+    const backupUrl = getBackupUrl(tenantName);
+    const authToken = generateToken(tenantName);
+
+    const backupData = fs.readFileSync(backupFile, "utf-8");
+
+    const res = await axios.post(backupUrl, {
+      hbs: backupData
+    }, {
+      headers: {
+        'Authorization': 'Bearer ' + authToken
+      }
+    });
+
+    console.log(res.status, res.data);
+  } catch (error) {
+    const message = error.response?.data || error.message;
+    console.error(message);
+  }
+}
+
+module.exports = {
+  getBackup, postBackup
+}
